70c3dbca46fec7a64f9426f1a68a781764dca76a
itspoma <itspoma@gmail.com>
2016-05-09 03:12:54 +0300

    added zoho-modules as constants into separate file

---
+++ ZohoModules.php
@@ -0,0 +1,28 @@
+<?php
+namespace Christiaan\ZohoCRMClient;
+
+/**
+ * ZohoModules
+ *
+ * @see https://www.zoho.com/crm/help/api/modules-fields.html
+ */
+class ZohoModules
+{
+  const MODULE_LEADS = "Leads";
+  const MODULE_ACCOUNTS = "Accounts";
+  const MODULE_CONTACTS = "Contacts";
+  const MODULE_POTENTIALS = "Potentials";
+  const MODULE_CAMPAIGNS = "Campaigns";
+  const MODULE_CASES = "Cases";
+  const MODULE_SOLUTIONS = "Solutions";
+  const MODULE_PRODUCTS = "Products";
+  const MODULE_PRICE = "Price";
+  const MODULE_QUOTES = "Quotes";
+  const MODULE_INVOICES = "Invoices";
+  const MODULE_SALES = "Sales";
+  const MODULE_VENDORS = "Vendors";
+  const MODULE_PURCHASE_ORDERS = "PurchaseOrders";
+  const MODULE_EVENTS = "Events";
+  const MODULE_TASKS = "Tasks";
+  const MODULE_CALLS = "Calls";
+}
